9b7b2063f9c8c9d438bc0c97c19b529cce1213f8
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-06-04 20:31:49 +0300

    Russian Localization

--- lua/entities/tempmod_freezeray/shared.lua
+++ lua/entities/tempmod_freezeray/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.PrintName = "Freezeray"
+ENT.PrintName = "#tempmod.tempmod_freezeray"
 ENT.Author = "Ty4a"
 ENT.Category = "Temperature Mod"
 ENT.Spawnable = true
--- lua/entities/tempmod_thermometer/shared.lua
+++ lua/entities/tempmod_thermometer/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.PrintName = "Thermometer"
+ENT.PrintName = "#tempmod.tempmod_thermometer"
 ENT.Author = "Ty4a"
 ENT.Category = "Temperature Mod"
 ENT.Spawnable = true
