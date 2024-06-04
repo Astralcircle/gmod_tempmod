ef68d3aface377788d40dd4c884020afbbdd5d84
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-06-04 18:22:25 +0300

    Removed useless client check

--- lua/autorun/client/halo.lua
+++ lua/autorun/client/halo.lua
@@ -4,27 +4,25 @@ hook.Add("PreDrawHalos", "AddHotMetalHalos", function()
         return
     end
 
-    if CLIENT then
-        local hotEntities = {}
-        local count = -1
+    local hotEntities = {}
+    local count = -1
 
-        for _, ent in ipairs(ents.FindByClass("prop_physics")) do
-            local temperature = ent:GetNW2Float("Temperature", 20)
-            if temperature >= 999 and ent:GetNWFloat("Temp_IsMetal") == 1 then
-                table.insert(hotEntities, ent)
-                count = count + 1
-                
-                if count > GetConVarNumber("tempmod_glow_max")-2 then
-                    break
-                end
+    for _, ent in ipairs(ents.FindByClass("prop_physics")) do
+        local temperature = ent:GetNW2Float("Temperature", 20)
+        if temperature >= 999 and ent:GetNWFloat("Temp_IsMetal") == 1 then
+            table.insert(hotEntities, ent)
+            count = count + 1
+            
+            if count > GetConVarNumber("tempmod_glow_max")-2 then
+                break
+            end
 
-                if temperature < 3000 then
-                    halo.Add(hotEntities, Color(255, 0, 0), 0.0025*temperature, 0.0025*temperature, 5, true, false)
-                    halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
-                else
-                    halo.Add(hotEntities, Color(255, 0, 0), 5, 5, 5, true, false)
-                    halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
-                end
+            if temperature < 3000 then
+                halo.Add(hotEntities, Color(255, 0, 0), 0.0025*temperature, 0.0025*temperature, 5, true, false)
+                halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
+            else
+                halo.Add(hotEntities, Color(255, 0, 0), 5, 5, 5, true, false)
+                halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
             end
         end
     end
