38afd8ffba682d938c36f876c5005349a3be5c3b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-08-05 21:36:46 +0300

    Delete lua directory

--- lua/entities/tempmod_heatray/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName = "#tempmod.tempmod_heatray"
-ENT.Author = "Ty4a"
-ENT.Category = "Temperature Mod"
-ENT.Spawnable = true
-ENT.AdminOnly = true
--- lua/entities/tempmod_thermometer/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName = "#tempmod.tempmod_thermometer"
-ENT.Author = "Ty4a"
-ENT.Category = "Temperature Mod"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
