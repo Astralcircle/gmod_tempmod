07a6786e66a0ce95fba6100733248b96e4a7649a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-08-10 10:51:05 +0300

    Add files via upload

--- lua/tempmod/modules/cl_halo.lua
+++ lua/tempmod/modules/cl_halo.lua
@@ -1,30 +1,44 @@
-hook.Add("PreDrawHalos", "AddHotMetalHalos", function()
-    if CLIENT then
-        if GetConVarNumber("tempmod_glow_enabled") == 0 then return end
-        local hotEntities = {}
-        local count = -1
-        local ply = LocalPlayer()
+local tempmod_glow_enabled = GetConVar("tempmod_glow_enabled")
+local tempmod_glow_max = GetConVar("tempmod_glow_max")
 
-        for _, ent in ipairs(ents.FindByClass("prop_physics")) do
-            if ent:GetNW2Bool("IsMetalObject") == true then
-            local temperature = ent:GetTemperature()
-            if temperature >= 999 then
-                table.insert(hotEntities, ent)
-                count = count + 1
-                
-                if count > GetConVarNumber("tempmod_glow_max")-2 then
-                    break
-                end
+local color_red = Color(255, 0, 0)
+local color_yellow = Color(255, 250, 0)
 
-                if temperature < 3000 then
-                    halo.Add(hotEntities, Color(255, 0, 0), 0.0025*temperature, 0.0025*temperature, 5, true, false)
-                    halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
-                else
-                    halo.Add(hotEntities, Color(255, 0, 0), 5, 5, 5, true, false)
-                    halo.Add(hotEntities, Color(255, 250, 0), 2, 2, 4, true, false)
-                end
-            end
+local function Halo()
+    local hotEntities = {}
+    local count = 0
+
+    for _, ent in ipairs(ents.FindByClass("prop_physics")) do
+        if not ent:GetNW2Bool("IsMetalObject") then return end
+
+        local temperature = ent:GetTemperature()
+
+        if temperature >= 999 then
+            count = count + 1
+            
+            if count > tempmod_glow_max:GetInt() then break end
+
+            table.insert(hotEntities, ent)
+
+            if temperature < 3000 then
+                halo.Add(hotEntities, color_red, 0.0025 * temperature, 0.0025 * temperature, 5, true, false)
+                halo.Add(hotEntities, color_yellow, 2, 2, 4, true, false)
+            else
+                halo.Add(hotEntities, color_red, 5, 5, 5, true, false)
+                halo.Add(hotEntities, color_yellow, 2, 2, 4, true, false)
             end
         end
+    end
+end
+
+if tempmod_glow_enabled:GetBool() then
+    hook.Add("PreDrawHalos", "AddHotMetalHalos", Halo)
+end
+
+cvars.AddChangeCallback("tempmod_glow_enabled", function(convar, old, new)
+    if tobool(new) then
+        hook.Add("PreDrawHalos", "AddHotMetalHalos", Halo)
+    else
+        hook.Remove("PreDrawHalos", "AddHotMetalHalos")
     end
 end)
