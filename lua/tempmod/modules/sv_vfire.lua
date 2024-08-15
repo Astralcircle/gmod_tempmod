28999feab26f2d41998205c05635137f257feac2
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-08-15 14:52:47 +0300

    rewrite modules

--- lua/tempmod/modules/sv_vfire.lua
+++ lua/tempmod/modules/sv_vfire.lua
@@ -1,16 +1,15 @@
-if vFireInstalled then
-    local TimeUpdate = CurTime()
-    hook.Add("Think", "vFireTemperatureSupport", function()
-        local curTime = CurTime()
-        for burningEnt, sameEnt in pairs (vFireGetBurningEntities()) do
-            if sameEnt:IsTemperatureAvaiable() then
-                if not TimeUpdate or curTime - TimeUpdate > 1 then
-                    local temp = sameEnt:GetTemperature()
-                    sameEnt:SetTemperature(temp+7)
+if not vFireInstalled then return end
 
-                    TimeUpdate = curTime + 1
-                end
-            end
-        end
-    end)
-end
+local delay = 0
+
+hook.Add("Think", "vFireTemperatureSupport", function()
+    if CurTime() < delay then return end
+
+    for _, ent in pairs(vFireGetBurningEntities()) do
+        if not ent:IsTemperatureAvaiable() then continue end
+
+        ent:SetTemperature(ent:GetTemperature() + 7)
+    end
+
+    delay = CurTime() + 1
+end)
