cb9e1d2154ddd012d6b555c5c4b8e95f8378e96b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-01-10 15:36:24 +0300

    Fix NULL error

--- lua/tempmod/modules/sv_vfire.lua
+++ lua/tempmod/modules/sv_vfire.lua
@@ -6,7 +6,7 @@ hook.Add("Think", "vFireTemperatureSupport", function()
     if CurTime() < delay then return end
 
     for _, ent in pairs(vFireGetBurningEntities()) do
-        if not ent:IsTemperatureAvaiable() then continue end
+        if not IsValid(ent) or not ent:IsTemperatureAvaiable() then continue end
 
         ent:SetTemperature(ent:GetTemperature() + 7)
     end
